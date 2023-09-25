501e3e0f89070e01c2d1198d1b437706e161ffe9
Ephraim Yisrael Jackson <71467747+ejackson725@users.noreply.github.com>
2023-09-25 01:23:43 +0100

    Update ripple_media.py

--- ripple_media.py
+++ ripple_media.py
@@ -25,12 +25,89 @@ def box_office_data():
 
     # 1.1 TODO: Convert the string `movies_str` into a list and assign it to a variable called `remaining_50_list`
     # NOTE: make sure that the commas that separate the titles in the string don't stay in the titles in the list!
+    #use a method to make a list
     # Print out the `type` of your new variable, then in a separate print statement, the number of items in it (should be 50).
+    
+    remaining_50_list = movies_str.split(",")
 
+    print(type(remaining_50_list))
+    print(len(remaining_50_list))
+    
+
+#     #1.2
     # 1.2 TODO: Hmm. Looks like `remaining_50_list` contains the movie "The Matrix" which is also in the "top_50_list".
     # Let's remove this duplicate title from `remaining_50_list`. 
+    
     # Let's use our conditional logic to check if "The Matrix" is still in `remaining_50_list`
     # Print `"The Matrix" in remaining_50_list`. It should print out `False`.
+    
+
+
+
+
+    remaining_50_list.remove("The Matrix")
+    
+
+
+
+    if "The Matrix" in remaining_50_list:
+        print("The Matrix")
+
+    else:
+        print(False)
+#1.3
+# 1.3 TODO: Let's add a movie to the end of the list called "Anywhere But Here".
+    # Let's use conditional logic again to check if the last movie in the list is now "Anywhere But Here".
+    # Use 'indexing' and conditional logic to check.
+
+        remaining_50_list.append ("Anywhere but here")
+        
+
+    if  "Anywhere but here" in remaining_50_list [-1]:
+        print(True)
+
+    else:
+        print(False)
+
+# #1.4
+ # 1.4 TODO: Let's make sure "remaining_50_list" has exactly 50 movie titles.
+    # Print out the number of title-strings in the list (count it with code!)
+    print(len(remaining_50_list))
+
+    
+    
+    
+    # 1.5 TODO: Great! Let's combine the two lists into one list called `top_100`.
+    # NOTE: Which list should be first when you combine them? The top 50 titles, or the remaining 50?
+    # Print out the number of title-strings in the list (count it with code!)
+
+
+
+    top_100  = top_50_list + remaining_50_list 
+    print(len(top_100))
+
+
+
+# 1.6 TODO: Let's print the first movie in our `top_100`list.
+    # Use 'indexing' to do this.
+
+    print(top_100[0])
+
+    #  # 1.7 TODO: Print the top 10 movies in `top_100` as a list.
+    # # Use 'slicing' to accomplish this!
+
+    print(top_100[0:10])
+
+
+# box_office_data()
+    
+    # 1.2 TODO: Hmm. Looks like `remaining_50_list` contains the movie "The Matrix" which is also in the "top_50_list".
+    # Let's remove this duplicate title from `remaining_50_list`. 
+    
+    # Let's use our conditional logic to check if "The Matrix" is still in `remaining_50_list`
+    # Print `"The Matrix" in remaining_50_list`. It should print out `False`.
+
+
     # NOTE: Make sure to double check the list you are removing from!
 
     # We're down to 49 movies in `remaining_50_list`.
@@ -52,7 +129,6 @@ def box_office_data():
     # 1.7 TODO: Print the top 10 movies in `top_100` as a list.
     # Use 'slicing' to accomplish this!
 
-
 def create_employee_email_address():
     # Production is going well, now you need to hire a sales person!
     # It's typical when you hire a new employee in your company to setup an email address for them
@@ -62,15 +138,48 @@ def create_employee_email_address():
 
     employee_name = 'Daffy Duck'
 
+
+
+
+
+
     # 2.1 TODO: Let's save the lowercase version of the employee_name in a new variable 'lower_name'
     # (use a string method to lower the name). Print out the variable.
 
-    # 2.2 TODO: We want to separate the first name and last name and save it in a variable 'names_list'
+    lower_name = employee_name.lower()
+    print(lower_name)
+
+
+# 2.2 TODO: We want to separate the first name and last name and save it in a variable 'names_list'
     # (use a string method to split the string into a list) Print out the variable.
+    names_list = lower_name.split(" ")
+    print(names_list)
+
+
+
+
+
+
+
+
 
     # 2.3 TODO: We want to join the first name and last name with a '.' and save it in a variable called
     # `joined_names` (use a string method to join the list into a new string) Print out the variable.
 
+    joined_names = ".".join(names_list)
+    print(joined_names)
+
+
+
+
+
+
+
+
     # 2.4 TODO: We want to add '@ripplemedia.com' to the end of the string inside joined_names and
     # save it in a variable `email` (use an f-string to combine the username with the email domain)
     # Print out the variable.
+    email = (f'{joined_names}@ripplemedia.com')
+    print(email)
+# assisted by Rico Marshall and Ulises SancheZ
+# create_employee_email_address()
